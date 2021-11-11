2788d4dc2d9c9d6793dcfcabff896019eb43932e
Lucas Guardese <lucas.guardese@gmail.com>
2021-11-11 19:23:15 -0300

    Adding last name and phone numbers to students

--- src/main/java/com/guardeselucas/StudentSystem/model/Student.java
+++ src/main/java/com/guardeselucas/StudentSystem/model/Student.java
@@ -12,7 +12,9 @@ public class Student {
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private int id;
     private String name;
+    private String lastName;
     private String address;
+    private String phoneNumber;
 
     public Student() {
     }
@@ -39,5 +41,22 @@ public class Student {
 
     public void setAddress(String address) {
         this.address = address;
+    }
+
+
+    public String getLastName() {
+        return lastName;
+    }
+
+    public void setLastName(String lastName) {
+        this.lastName = lastName;
+    }
+
+    public String getPhoneNumber() {
+        return phoneNumber;
+    }
+
+    public void setPhoneNumber(String phoneNumber) {
+        this.phoneNumber = phoneNumber;
     }
 }
--- studentfrontend/src/components/Student.js
+++ studentfrontend/src/components/Student.js
@@ -9,12 +9,14 @@ import AccountCircleIcon from '@mui/icons-material/AccountCircle';
 export default function Student() {
     const paperStyle = { padding: '50px 20px', width: 600, margin: '20px auto' }
     const [name, setName] = React.useState('')
+    const [lastName, setLastName] = React.useState('')
+    const [phoneNumber, setPhoneNumber] = React.useState('')
     const [address, setAddress] = React.useState('')
     const [students, setStudents] = React.useState([])
 
     const handleClick = (e) => {
         e.preventDefault()
-        const student = { name, address }
+        const student = { name, address, lastName, phoneNumber }
         console.log(student)
 
         fetch("http://localhost:8080/student/add", {
@@ -60,14 +62,23 @@ export default function Student() {
                     noValidate
                     autoComplete="off"
                 >
-                    <TextField id="standard-basic" label="Student Name" variant="standard" fullWidth
+                    <TextField id="standard-basic" label="Name" variant="standard" fullWidth
                         value={name}
                         onChange={(e) => setName(e.target.value)}
                     />
-                    <TextField id="standard-basic" label="Student Adress" variant="standard" fullWidth
+                    <TextField id="standard-basic" label="Last Name" variant="standard" fullWidth
+                        value={lastName}
+                        onChange={(e) => setLastName(e.target.value)}
+                    />
+                    <TextField id="standard-basic" label="Adress" variant="standard" fullWidth
                         value={address}
                         onChange={(e) => setAddress(e.target.value)}
                     />
+                    <TextField id="standard-basic" label="Phone Number" variant="standard" fullWidth
+                        value={phoneNumber}
+                        onChange={(e) => setPhoneNumber(e.target.value)}
+                    />
+                    
                     <Button color="success" onClick={handleClick} variant="contained" >Submit</Button>
                 </Box>
             </Paper>
@@ -77,10 +88,12 @@ export default function Student() {
                 {students.map(student => (
                     <Paper elevation={3} style={{ margin: "10px", padding: "15px", textAlign: "left" }} key={student.id}>
 
-                        <AccountCircleIcon /><br />
+                        <AccountCircleIcon /><br /><br />
                         <b>Id: </b>{student.id}<br />
                         <b>Name: </b>{student.name}<br />
+                        <b>Last Name: </b>{student.lastName}<br />
                         <b>Address: </b>{student.address}<br />
+                        <b>Phone Number: </b>{student.phoneNumber}<br />
                         <IconButton aria-label="delete" id="delete" onClick={(e) => { handleDelete(e, student.id) }}>
                             <DeleteIcon />
                         </IconButton>
